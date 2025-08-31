944164b494cb6e703efa2ed7fa42c27b708b5de0
Maxime Boucher <maxime@maximeboucher.com>
2025-08-31 12:07:04 -0600

    Remove console log

--- components/PassphraseForm.tsx
+++ components/PassphraseForm.tsx
@@ -36,8 +36,6 @@ export default function PassphraseForm({
   const router = useRouter();
   const { set } = useDek();
 
-  console.log("userPrefs", userPrefs);
-
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
